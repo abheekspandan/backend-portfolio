2bc349b1ef816a853d90492ed5ce76e838a17ccb
abheekspandan <109303545+abheekspandan@users.noreply.github.com>
2025-02-05 13:55:40 +0530

    Update server.js

--- myserver/server.js
+++ myserver/server.js
@@ -31,8 +31,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
